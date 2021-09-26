8be067753cca896bfa5f0cd0f21d20eee0e2b6d8
Tekh-ops <jugger.nut2234@gmail.com>
2021-09-26 21:59:17 +0530

    🪟 Rendering Multiple Objects in Window

--- src/Application.cpp
+++ src/Application.cpp
@@ -66,10 +66,10 @@ int main()
     {
 
         float positions[] = {
-            100.0f , 100.0f  , 0.0f , 0.0f, // 0
-            200.0f , 100.0f  , 1.0f , 0.0f, // 1
-            200.0f ,  200.0f  , 1.0f , 1.0f, // 2
-            100.0f ,  200.0f  , 0.0f , 1.0f  // 3
+            -50.0f , -50.0f  , 0.0f , 0.0f, // 0
+             50.0f , -50.0f  , 1.0f , 0.0f, // 1
+             50.0f ,  50.0f  , 1.0f , 1.0f, // 2
+            -50.0f ,  50.0f  , 0.0f , 1.0f  // 3
         };
 
         unsigned int indices[] = {
@@ -91,8 +91,8 @@ int main()
         IndexBuffer ib(indices, 6);
 
         // These values are used to calculate the aspect ratio of the window
-        glm::mat4 proj = glm::ortho(0.0f, 960.0f, 0.0f,540.0f, -1.0f, 1.0f); // left, right, bottom, top, near, far
-        glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(-100, 0, 0));
+        glm::mat4 proj = glm::ortho(0.0f, 960.0f, 0.0f, 540.0f, -1.0f, 1.0f); // left, right, bottom, top, near, far
+        glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(0, 0, 0));
 
         Shader shader("res/shaders/Basic.shader");
         shader.Bind();
@@ -117,7 +117,8 @@ int main()
         ImGui_ImplGlfwGL3_Init(window, true);
         ImGui::StyleColorsDark();
 
-        glm::vec3 translation(200, 200, 0);
+        glm::vec3 translationA(200, 200, 0);
+        glm::vec3 translationB(400, 200, 0);
 
         /* Loop until the user closes the window */
         while (!glfwWindowShouldClose(window))
@@ -125,23 +126,28 @@ int main()
             /* Render here */
             renderer.Clear();
 
-            glfwPollEvents();
             ImGui_ImplGlfwGL3_NewFrame();
 
-            glm::mat4 model = glm::translate(glm::mat4(1.0f), translation);
-            glm::mat4 mvp = proj * view * model;
+            {
+                glm::mat4 model = glm::translate(glm::mat4(1.0f), translationA);
+                glm::mat4 mvp = proj * view * model;
+                shader.Bind();
+                shader.SetUniformMat4f("u_MVP", mvp);
 
-            // glClearColor(0.2f, 0.3f, 0.3f, 1.0f); // changes color of screen
+                renderer.Draw(va, ib, shader);
+            }
 
+            glClearColor(0.2f, 0.3f, 0.3f, 1.0f); // changes color of screen
             // shader.SetUniform4f("bgColor", 0.2f, 0.3f, 0.3f, 1.0f);
 
-            shader.Bind();
-            shader.SetUniform4f("u_Color",r, 0.3f, 0.8f, 1.0f);
-            shader.SetUniformMat4f("u_MVP", mvp);
+            {
+                glm::mat4 model = glm::translate(glm::mat4(1.0f), translationB);
+                glm::mat4 mvp = proj * view * model;
+                shader.Bind();
+                shader.SetUniformMat4f("u_MVP", mvp);
 
-
-            renderer.Draw(va, ib, shader); // draw the triangle
-            GLCall(glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr));
+                renderer.Draw(va, ib, shader);
+            }
 
             if (r >= 1.0f)
                 increment = -0.05f;
@@ -151,7 +157,8 @@ int main()
             r += increment;
 
         {
-            ImGui::SliderFloat3("Translation", &translation.x, 0.0f, 960.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
+            ImGui::SliderFloat3("Translation A", &translationA.x, 0.0f, 960.0f);
+            ImGui::SliderFloat3("Translation B", &translationB.x, 0.0f, 960.0f);
 
             ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
         }
